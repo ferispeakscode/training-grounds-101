eefdd01d5083618e964465f53030f37794f8d917
aDiZmalCode <deferdinand@gmail.com>
2021-11-11 23:06:56 -0400

    some form fields and buttons

--- src/components/BasicForm.vue
+++ src/components/BasicForm.vue
@@ -1,8 +1,23 @@
 <template>
   <h1>Basic form with configuration {{ config }} will go here.</h1>
-  <label>User's Name</label>
-  <input type="text" v-model="user">
+  <form>
+    <label>Name</label>
+    <input type="text" v-model="user">
+    <label>Gender</label>
+    <select v-model="sex">
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+      <option value="Confused">Other</option>
+    </select>
+    <label>Age</label>
+    <input type="number" size="100" min="1" max="100" step="1" v-model="age">
+    <button>Submit</button>
+    <button>Delete</button>
+  </form>
+  
   <p>{{user}}</p>
+  <p>{{sex}}</p>
+  <p>{{age}}</p>
 </template>
 
 <script>
@@ -10,7 +25,9 @@ export default {
   name: 'BasicForm',
   data() {
     return{
-      user: ''
+      user: '',
+      sex: 'select',
+      age: null
     }
   },
   props: {
@@ -23,5 +40,22 @@ export default {
 <style scoped>
 h1 {
     color: red;
+}
+input, select {
+  border: none;
+  border-bottom: solid;
+  }
+label {
+  text-transform: uppercase;
+  display: inline-block;
+}
+form {
+  width: 420px;
+}
+button {
+  background: grey;
+}
+button:hover {
+  background: rgb(58, 250, 186);
 }
 </style>
