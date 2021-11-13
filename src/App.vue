4a596ba8ab2d0a28e5354d9d425a980a899b8f3e
Feri <ferispeakscode@gmail.com>
2021-11-13 13:57:19 -0500

    Install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <BasicForm type="A"/>
-  <BasicForm type="B"/>
-  <BasicForm type="C"/>
-  <HelloWorld msg="Welcome to Training Grounds 101"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import BasicForm from './components/BasicForm.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    BasicForm
-  }
-}
-</script>
 
 <style>
 #app {
@@ -26,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <BasicForm type="A"/>
+    <BasicForm type="B"/>
+    <BasicForm type="C"/>
+    <HelloWorld msg="Welcome to Training Grounds 101"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import BasicForm from '@/components/BasicForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    BasicForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
