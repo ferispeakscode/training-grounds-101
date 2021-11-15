37f0852d6869af239b06a682b91089fc0b85a9fd
aDiZmalCode <deferdinand@gmail.com>
2021-11-14 21:11:44 -0400

    Added a form new component

---
+++ src/components/CatalogFormat.vue
@@ -0,0 +1,54 @@
+<template>
+<form>
+  <div class="catTitle" @click="toggleDetails">
+      <h3>{{ catHeader }}</h3>
+  </div>
+  <div class="catDetails" v-if="details">
+          <textarea v-model="catBody"></textarea>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            catHeader: 'Test',
+            catBody: 'Lets see if this works',
+            details: false
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.details = !this.details
+        }
+    }
+
+}
+</script>
+
+<style>
+    form {
+        max-width: 680px;
+        width: 100%;
+        display: inline-block;
+        border: none;
+        border-radius: 6px;
+        background: whitesmoke;
+        padding: 10px;
+        text-transform: uppercase;
+        text-align: left;
+        border-left: solid rgb(255, 103, 2);
+    }
+    textarea {
+        border: none;
+        width: 100%;
+        height: 100px;
+    }
+    .catTitle {
+        color: black;
+    }
+    .catDetails {
+        border-bottom: solid rgb(255, 103, 2);
+    }
+</style>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -3,10 +3,18 @@
       <img alt="Vue logo" src="../assets/logo.png">
       <h1>Welcome to our catalog</h1>
   </div>
+  <div>
+      <CatalogFormat />
+  </div>
 </template>
 
 <script>
+import CatalogFormat from '@/components/CatalogFormat.vue'
+
 export default {
+    components: {
+        CatalogFormat
+    }
 
 }
 </script>
